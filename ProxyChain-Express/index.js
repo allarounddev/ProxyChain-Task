e794629ceb84fc0cd9a1454c3f36e679b9f8247a
phantomdev99 <venushappy815@gmail.com>
2023-05-23 21:39:41 +0900

    fix issues

--- ProxyChain-Express/index.js
+++ ProxyChain-Express/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const url = require('url')
 
 app.use(cors())
 
 app.get('/jpg-image', (req, res) => {
   // Return a dummy JPG image
-  res.sendFile('dummy.jpg', { root: __dirname });
+  // res.sendFile('', { root: __dirname });
+  const imgUrl = 'https://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg'
+  const options = url.parse(imgUrl)
+  console.log(options.href)
+  // res.sendFile(imgUrl)
+  res.redirect(imgUrl);
+
 });
 
 app.get('/not-image', (req, res) => {
--- ProxyChain/index.js
+++ ProxyChain/index.js
@@ -2,42 +2,113 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const sizeOf = require('image-size');
 const http = require('http')
+const https = require('https')
+const sharp = require('sharp');
+const cheerio = require('cheerio');
+const zlib = require('zlib');
 
 const app = express();
 
 // Create the proxy middleware
 const proxyMiddleware = createProxyMiddleware({
-  target: 'http://127.0.0.1:8080/', // Replace with the target URL you want to proxy
+  target: 'https://oripa.clove.jp/', // Replace with the target URL you want to proxy
   changeOrigin: true, // Changes the origin of the host header to the target URL
+  selfHandleResponse: true,
   onProxyRes: async (proxyRes, req, res) => {
     const contentType = proxyRes.headers['content-type'];
-    console.log(contentType);
-    if (contentType && contentType.startsWith('image/')) {
-      // Intercept image requests
 
-      // Get the dimensions of the image without downloading the entire file
-      console.log('http://127.0.0.1:8080' + req.url);
-      const size = await getImageSize(req.url);
+    if (true) {
+      try {
+        const chunks = [];
 
-      if (size) {
-        // Generate a dummy image with the same dimensions
-        const dummyImage = generateDummyImage(size.width, size.height);
+        // Collect the response data chunks
+        proxyRes.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
 
-        return {
-          customResponseFunction: () => {
-            return {
-              statusCode: 200,
-              body: dummyImage,
-              headers: {
-                'Content-Type': 'image/jpeg',
-              },
-            };
-          },
-        };
+        // Wait for the response to finish
+        await new Promise((resolve) => {
+          proxyRes.on('end', resolve);
+        });
+
+        // Combine the response data chunks into a single buffer
+        const responseData = Buffer.concat(chunks);
+
+        if (contentType && contentType.includes('image')) {
+          console.log('something');
+          const sharp = require('sharp');
+
+          const width = 500; // Replace with your desired width
+          const height = 500; // Replace with your desired height
+
+          sharp({
+            create: {
+              width: width,
+              height: height,
+              channels: 4,
+              background: { r: 128, g: 128, b: 128, alpha: 1 }
+            }
+          })
+            .png()
+            .toBuffer()
+            .then((buffer) => {
+              const dummyImageBase64 = buffer.toString('base64');
+
+              // Set the appropriate headers and send the response
+              res.setHeader('content-type', 'image/png');
+              res.send(Buffer.from(dummyImageBase64, 'base64'));
+            })
+            .catch((error) => {
+              console.error('Failed to create dummy image:', error);
+              res.sendStatus(500);
+            });
+            return;
+        }
+        // Convert the buffer to a string using the appropriate encoding
+        // const encoding = proxyRes.headers['content-encoding'] || 'utf-8';
+        // const htmlBody = responseData.toString('utf-8');
+        try{
+          htmlBody = zlib.gunzipSync(responseData).toString('utf8');
+        } catch(err) {
+          htmlBody= responseData.toString('utf8');
+        }
+
+        if(htmlBody.indexOf("<img") < 0) {
+          proxyRes.pipe(res);
+          return;
+        }
+
+        // Parse the HTML body using Cheerio
+        const $ = cheerio.load(htmlBody);
+
+        // Replace all <img> tags with a dummy image of the same size and solid gray color
+        $('img').each((index, element) => {
+          const imgWidth = $(element).attr('width');
+          const imgHeight = $(element).attr('height');
+          const dummyImage = `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkwAIYBAAUDAAK+AAPbDjkRAAAAABJRU5ErkJggg==`;
+
+          $(element).attr('src', dummyImage);
+          $(element).attr('width', imgWidth);
+          $(element).attr('height', imgHeight);
+          $(element).css('background-color', 'gray');
+        });
+
+        // Get the modified HTML
+        const modifiedHtml = $.html();
+
+        // Set the modified HTML as the response
+        res.setHeader('content-type', 'text/html');
+        res.send(modifiedHtml);
+      } catch (error) {
+        console.error('Error handling proxy response:', error);
+        res.sendStatus(500);
       }
+    } 
+    else {
+      // For other content types, forward the response as-is
+      // res.setHeader('content-type', contentType);
+      proxyRes.pipe(res);
     }
-    // Continue with the response
-    return proxyRes;
   }
 });
 
@@ -49,31 +120,59 @@ app.listen(3000, () => {
   console.log('Proxy server is running on port 3000');
 });
 
-function getImageSize(url) {
-  return new Promise((resolve, reject) => {
-    http.get(url, (response) => {
-      const chunks = [];
-
-      response.on('data', (chunk) => {
-        chunks.push(chunk);
-      }).on('end', () => {
-        const buffer = Buffer.concat(chunks);
-        const size = imageSize(buffer);
-        resolve(size);
-      }).on('error', (error) => {
-        reject(error);
+async function getImageSize(url) {
+  return await new Promise((resolve, reject) => {
+    if(url.startsWith("https")) {
+      https.get(url, (response) => {
+        const chunks = [];
+  
+        response.on('data', (chunk) => {
+          chunks.push(chunk);
+        }).on('end', () => {
+          const buffer = Buffer.concat(chunks);
+          const dimensions = sizeOf(buffer);
+          resolve(dimensions);
+        }).on('error', (error) => {
+          reject(error);
+        });
       });
-    });
+    }
+    else {
+      http.get(url, (response) => {
+        const chunks = [];
+
+        response.on('data', (chunk) => {
+          chunks.push(chunk);
+        }).on('end', () => {
+          const buffer = Buffer.concat(chunks);
+          const dimensions = sizeOf(buffer);
+          resolve(dimensions);
+        }).on('error', (error) => {
+          reject(error);
+        });
+      });
+    }
   });
 }
 
 // Helper function to generate a dummy image with specified dimensions
 function generateDummyImage(width, height) {
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  const context = canvas.getContext('2d');
-  context.fillStyle = 'gray';
-  context.fillRect(0, 0, width, height);
-  return canvas.toDataURL('image/jpeg');
+  const imageWidth = parseInt(width);
+  const imageHeight = parseInt(height);
+
+  const imageBuffer = Buffer.from(`<svg xmlns="http://www.w3.org/2000/svg" width="${imageWidth}" height="${imageHeight}"/>`);
+
+  sharp(imageBuffer)
+    .png()
+    .toBuffer()
+    .then((data) => {
+      return data; 
+      // res.set('Content-Type', 'image/png');
+      // res.send(data);
+    })
+    .catch((error) => {
+      // console.error('Error:', error);
+      // res.sendStatus(500);
+      return null;
+    });
 }
