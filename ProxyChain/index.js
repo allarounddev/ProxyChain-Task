b4d9a20a104fb35e0149b78c8b3055197e940a35
allarounddev <allarounddeveloper@outlook.com>
2023-05-25 01:27:56 +0900

    proxy server updated

--- ProxyChain/index.js
+++ ProxyChain/index.js
@@ -6,69 +6,44 @@ const https = require('https')
 const sharp = require('sharp');
 const cheerio = require('cheerio');
 const zlib = require('zlib');
-
+const ProxyChain = require('proxy-chain');
 const app = express();
+const probe = require('probe-image-size');
 
-
-const proxyMiddleware = createProxyMiddleware({
-  target: 'http://127.0.0.1:8080/', // Replace with the target URL you want to proxy
-  changeOrigin: true, // Changes the origin of the host header to the target URL
-  onProxyRes: async (proxyRes, req, res) => {
-    const contentType = proxyRes.headers['content-type'];
-
-    if (proxyRes) {
-      // Intercept image requests
-
-      // Get the dimensions of the image without downloading the entire file
-      console.log('http://127.0.0.1:8080' + req.url);
-      const size = await getImageSize('http://127.0.0.1:8080' + req.url);
-      console.log(size)
-      if (size) {
-        // Generate a dummy image with the same dimensions
-        const dummyImage = generateDummyImage(size.width, size.height);
+const server = new ProxyChain.Server({
+  port: 3000,
+  prepareRequestFunction: async ({ request, username, password, hostname, port, isHttp }) => {
+    const size = await getImageSize('http://res.cloudinary.com/demo/image/upload/v1312461204/sample.jpg');
+    console.log(size.height)
+    if ((request.url == "/jpg-image") && (size)) {
+      const dummyImage = generateDummyImage(size.width, size.height);
         return {
-          customResponseFunction: () => {
-            return {
-              statusCode: 200,
-              body: dummyImage,
-              headers: {
-                'Content-Type': 'image/jpeg',
-              },
-            };
-          },
+            customResponseFunction: () => {
+                return {
+                    statusCode: 200,
+                    upstreamProxyUrl: `http://127.0.0.1:3000`,
+                    body: "Saved dummy image",
+                    // headers: {
+                    //   'Content-Type': 'image/*',
+                    // },
+                };
+            },
         };
-      }
     }
-    // Continue with the response
-    return proxyRes;
-  }
+  },
 });
 
-// Proxy all requests to the target server
-app.use('/', proxyMiddleware);
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Proxy server is running on port 3000');
+server.listen(() => {
+  console.log(`Proxy server is listening on port ${server.port}`);
 });
 
-function getImageSize(url) {
+
+async function getImageSize(url) {
   console.log(url)
-  return new Promise((resolve, reject) => {
-    http.get(url, (response) => {
-      const chunks = [];
-
-      response.on('data', (chunk) => {
-        chunks.push(chunk);
-      }).on('end', () => {
-        const buffer = Buffer.concat(chunks);
-        const size = sizeOf(buffer);
-        resolve(size);
-      }).on('error', (error) => {
-        reject(error);
-      });
-    });
-  });
+  let result = await probe(url);
+  console.log(result)
+  return result
 }
 
 
@@ -77,19 +52,13 @@ function generateDummyImage(width, height) {
   const imageWidth = parseInt(width);
   const imageHeight = parseInt(height);
 
-  const imageBuffer = Buffer.from(`<svg xmlns="http://www.w3.org/2000/svg" width="${imageWidth}" height="${imageHeight}"/>`);
+  // const imageBuffer = Buffer.from(`<svg xmlns="https://svgsilh.com/svg_v2/1801287.svg" width="${imageWidth}" height="${imageHeight}"/>`);
+  console.log("generateDummyImage")
 
-  sharp(imageBuffer)
-    .png()
-    .toBuffer()
-    .then((data) => {
-      return data; 
-      // res.set('Content-Type', 'image/png');
-      // res.send(data);
-    })
-    .catch((error) => {
-      // console.error('Error:', error);
-      // res.sendStatus(500);
-      return null;
-    });
+  const image = sharp('./tempdummy.jpg').
+  resize(imageWidth,imageHeight).
+  flatten().
+  toFile('newFile.jpg');
+
+  return image
 }
